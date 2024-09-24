eb3fd8b5b3f3ab4b7e74ceb72413fd516fe145fc
Fred <fredcagong1234567890@gmail.com>
2024-09-25 04:27:19 +0800

    Completed Activity 3: Eloquent ORM Basics

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,63 @@
 <?php
 
+use App\Models\Student;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/students/create', function () {
+	$student = new Student();
+	$student->first_name = 'John';
+	$student->last_name = 'Doe';
+	$student->email = 'johndoe@example.com';
+	$student->age = 22;
+	$student->save();
+	return 'Student Created!';
+});
+
+Route::get('/students', function () {
+	$students = Student::all();
+	return $students;
+});
+
+Route::get('/students/update', function () {
+	$student = Student::where('email', 'johndoe@example.com')->first();
+	$student->email = 'john.doe@newmail.com';
+	$student->age = 23; //Update age as well
+	$student->save();
+	return 'Student Updated!';
+});
+
+Route::get('/students/delete', function () {
+	$student = Student::where('email', 'john.doe@newmail.com')->first();
+	$student->delete();
+	return 'Student Deleted!';
+});
+
+Route::get('/courses/create', function () {
+	$course = new Course();
+	$course->course_name = 'Introduction to Databases';
+	$course->save();
+	return 'Course Created!';
+});
+
+Route::get('/course/{id}/students', fucntion ($id) {
+	$course = Course::find($id);
+	return $course->students;
+});
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
 
 /*
 |--------------------------------------------------------------------------
